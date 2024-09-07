e3e8b67dafa6b3e6654b20e0b0e0b377e6a6799b
DeLance Schmidt <dschmidt1@delances-air.lan>
2024-09-07 09:02:14 -0600

    Adding basic debugging configuration for the project.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    port: 8090,
+    proxy: {
+      "/api/v1": {
+        // Path to API from KTOR server.
+        target: "http://localhost:8080", // This the default URL for the KTOR Application
+        changeOrigin: true, // Required for CORS handling
+      },
+    },
+  },
+  configureWebpack: {
+    devtool: "eval-source-map",
+  },
 });
